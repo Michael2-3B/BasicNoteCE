1c4ca11235244cfc4a814c4b12f4119320baf01f
Michael2-3B <34347626+Michael2-3B@users.noreply.github.com>
2018-06-25 15:25:00 -0500

    Update main.js

--- Txt2Note/main.js
+++ Txt2Note/main.js
@@ -20,15 +20,21 @@ function convertTxt(){
     		text=text.substr(0,i)+"\\"+text.substr(i);
 	}
     for(var i=2;i<text.length;i+=2){
-    	if (text[i]=='"')
+    	if (text[i]=='"'){
     		text = text.substr(0,i)+"'"+text.substr(i+1);
-        if (text[i]=="\n"){
-			text = text.substr(0,i-1)+"[xbar]"+text.substr(i+1);
-        } else {
-        	if (dictionary.indexOf(text[i]) == -1){
-				text = text.substr(0,i)+"?"+text.substr(i+1);
-            }
-        }
+		} else {
+        	if (text[i]=="\n"){
+				text = text.substr(0,i-1)+"[xbar]"+text.substr(i+1);
+        	} else {
+				if (text[i]=="\t"){
+					text = text.substr(0,i)+" \ \ \ "+text.substr(i+1);
+				} else {
+        			if (dictionary.indexOf(text[i]) == -1){
+						text = text.substr(0,i)+"?"+text.substr(i+1);
+					}
+				}
+			}
+		}
     }
     text=" "+text;
 	return text;
