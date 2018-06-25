d18513fd643763393bdba58bfb893b7b40dfcbd5
Michael2-3B <34347626+Michael2-3B@users.noreply.github.com>
2018-06-25 15:16:45 -0500

    Update main.js

--- Txt2Note/main.js
+++ Txt2Note/main.js
@@ -26,7 +26,7 @@ function convertTxt(){
 			text = text.substr(0,i-1)+"[xbar]"+text.substr(i+1);
         } else {
         	if (dictionary.indexOf(text[i]) == -1){
-				text = text.substr(0,i-1)+text.substr(i+1);
+				text = text.substr(0,i)+"?"+text.substr(i+1);
             }
         }
     }
